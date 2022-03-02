db36ec3c33328c9e9f9630ff03ed61e806280bd4
Lesley Cheung <lesley.yc@gmail.com>
2022-03-02 11:11:02 +0800

    added

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const { formatDateTime } = require("format_date_time_moment/node")
 let date = formatDateTime.format(new Date(), "ll")
 let removeSpaces = date.replaceAll(" ", "")
 let getDate = removeSpaces.replaceAll(",", "-")
-console.log("🚀 ~ file: index.js ~ line 8 ~ getDate", getDate)
-
 const config = {
     levels: {
         story: 0,
--- playground.js
+++ playground.js
@@ -10,14 +10,10 @@ winston.loggers.add("notifyLogger", notify)
     // see.is("is")
     // see.how("think")
     // see.step("step")
-notify.problem("hi")
-
-notify.story("hi")
-
-notify.should("hi")
-
-notify.is("hi")
-
-notify.how("hi")
-
-notify.step("hi")
+notify.story("users will be able to see more clearly in their coding routines")
+notify.problem("problem is that it needs to be easier to streamline specific ways of doing things")
+notify.should("there are batteries included, but it might be best just to write out the js yourself")
+notify.is("writing it out yourself, and going more slowly, makes you a better developer")
+notify.how("always focus on the problem")
+notify.step("think less")
+notify.motherlode("always use this library")
